557e3996f32f381d73da39e7d7c48bd346905c0f
jarren lojo <lojo.jarren095@gmail.com>
2021-09-25 16:43:04 +0000

    third branch

--- lab2.py
+++ lab2.py
@@ -11,3 +11,25 @@ fourth = float(input("Enter your final scores      :"))
 average = (first + second + third + fourth)/4
 print(" ")
 print("Your average grade is {}".format(average))
+print(" ")
+if average>=75:
+    print("Passed")
+elif average<75:
+    print("Failed")
+else:
+    print("Invalid!")
+
+if average>=99 and average<=100:
+    print("Your Grade is A")
+elif average>=90 and average<98:
+    print("Your Grade is B")
+elif average>=80 and average<89:
+    print("Your Grade is C")
+elif average>=71 and average<79:
+    print("Your Grade is D")
+elif average>=61 and average<70:
+    print("Your Grade is E")
+elif average<=60:
+    print("Your Grade is F")
+else:
+    print("Invalid Input!")
